4f790ac4d028f12bff5cfd4f9e5bec203115b69e
Chodanun Srinil <step_nilanam@hotmail.com>
2017-03-05 14:50:56 +0700

    start wotdcut

--- preprocess.py
+++ preprocess.py
@@ -6,30 +6,39 @@ def readFile(path):
 def writeFile(path):
 	return open(path,'w')
 
-def preprocessing(file):
-	# possible_words = ['อ','น']
+def preprocessing(file,func_write=False):
+	except_words = [',',"\"",' ','ด']
+	if func_write :
+		newfile = writeFile('./data/comments-removing-redundant.csv')
+	
 	for i in file:
-		print (i)
+		comment_new =""
 		for j in range(len(i)):
-			if j > 0 and j < len(i)-1:
-				if i[j] == i[j-1] and i[j] == i[j+1] :
-					pass
-				else :
-					print (i[j],end='')
+			if i[j] in except_words or i[j].isdigit():
+				comment_new+=i[j]
 			else :
-				print (i[j],end='')
-			# elif i[j] == i[j-1]:
-			# 	if i[j] in possible_words :
-			# 		print (i[j],end='')
+				if j > 0 and j < len(i)-1:
+					if i[j] == i[j-1] and i[j] == i[j-2] and j>1 :
+						pass
+					elif i[j] != i[j-1] or i[j] != i[j+1] :
+						comment_new+=i[j]
+				else :
+					comment_new+=i[j]
 
-
-		input()
+		if comment_new != i :
+			print (i)
+			print (comment_new)
+		if func_write :
+			newfile.write("%s"%comment_new)	
+	# close file
+	if func_write :
+		newfile.close()
 	
 
 def main():
 	# 1: preprocessing step
 	comments = readFile('./data/comments.csv')
-	comments = preprocessing(comments)
+	comments = preprocessing(comments,func_write=False)
 
 
 if __name__ == '__main__' :
